7e43b5cf56ee7320dfef6f39012382eba87b5767
Eduardo da silva albuquerque <eduardosilvaalb26@gmail.com>
2026-06-28 12:53:20 -0300

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-    
+      // implement node event listeners here
     },
-    baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php'
   },
 });
